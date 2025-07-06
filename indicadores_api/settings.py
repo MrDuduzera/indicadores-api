40332a7eac874e67fb4468eaa1f46357db19d3c1
MrDuduzera <eduardo.pereira@aluno.fmpsc.edu.br>
2025-07-06 19:25:12 -0300

    Ajusta headers e métodos CORS para aceitar preflight

--- indicadores_api/settings.py
+++ indicadores_api/settings.py
@@ -135,4 +135,25 @@ REST_FRAMEWORK = {
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 
